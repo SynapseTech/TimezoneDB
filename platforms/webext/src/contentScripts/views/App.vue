99c3c4d2552190bff41d720474efccf5562299e6
Liz Ainslie <lizainslie16@gmail.com>
2022-05-23 21:45:14 -0700

    Add browser extension target

---
+++ platforms/webext/src/contentScripts/views/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import 'virtual:windi.css'
+</script>
+
+<template>
+
+</template>
---
+++ platforms/webext/src/options/Options.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { storageDemo } from '~/logic/storage'
+</script>
+
+<template>
+  <main class="bg-gray-200 p-4">
+    <h1>Options</h1>
+    <input v-model="storageDemo" class="border border-gray-400 rounded px-2 py-1 mt-2">
+  </main>
+</template>
