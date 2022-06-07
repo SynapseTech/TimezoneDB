22631467ea3412a44e9e73ab6642bad79b331fc0
Liz Ainslie <lizainslie16@gmail.com>
2022-06-07 07:58:30 -0700

    Extract button styles as common

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -3,7 +3,22 @@ import Page from "../components/Page.vue";
 </script>
 
 <template>
-  <Page>
+  <Page nobg>
+    <header class="h-[50vh] w-full flex flex-col items-center justify-center bg-gray-200 border-gray-300 border-t-[1px] border-opacity-10">
+      <h1 class="font-light text-5xl">TimezoneDB</h1>
+      <h2 class='mt-2 text-gray-600 text-xl'>An easy, cross-platform method of keeping track of other people's timezones.</h2>
+    </header>
 
+    <main class="container mx-auto">
+
+    </main>
   </Page>
 </template>
+
+<style scoped lang="scss">
+@import '../styles/button.scss';
+
+.platformCard {
+
+}
+</style>
