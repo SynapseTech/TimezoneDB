a4135c860714caf134861d22b9cbcef2dfa7b415
Liz Ainslie <lizainslie16@gmail.com>
2022-06-07 07:52:21 -0700

    Add nobg option to page

--- web/src/components/Page.vue
+++ web/src/components/Page.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-import Navbar from "./Navbar.vue";
-import Footer from "./Footer.vue";
+import Navbar from './Navbar.vue';
+import Footer from './Footer.vue';
+import { defineProps, withDefaults, reactive, computed } from 'vue';
+
+interface PageProps {
+  nobg?: boolean
+}
+
+const props = withDefaults(defineProps<PageProps>(), { nobg: false })
+const { nobg } = reactive(props);
+
+const pageClass = computed(() => ({
+  nobg,
+}))
 </script>
 
 <template>
-  <div class="page">
+  <div class="page" :class="pageClass">
     <Navbar />
     <div class="content">
       <slot />
@@ -16,6 +28,10 @@ import Footer from "./Footer.vue";
 <style scoped lang="scss">
 .page {
   @apply min-h-screen flex flex-col bg-gray-200;
+
+  &.nobg {
+    @apply bg-transparent;
+  }
 }
 
 .content {
