9407faf4de099e274030b72059aa87cb4b1109b6
Liz Ainslie <lizainslie16@gmail.com>
2022-06-16 21:15:33 -0700

    Organize dependencies & add dependabot config for powercord

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -23,6 +23,7 @@ application {
 }
 
 dependencies {
+    // Ktor
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-locations-jvm:$ktor_version")
@@ -34,13 +35,18 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-sessions-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.litote.kmongo:kmongo-serialization:4.6.0")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("org.litote.kmongo:kmongo-serialization:4.6.0") // MongoDB
+    implementation("xyz.downgoon:snowflake:1.0.0") // ID generation
+    implementation("com.google.guava:guava:31.1-jre") // Guava
+
+    // Testing
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation("xyz.downgoon:snowflake:1.0.0")
-    implementation("com.google.guava:guava:31.1-jre")
 }
 
 tasks.withType<Jar> {
