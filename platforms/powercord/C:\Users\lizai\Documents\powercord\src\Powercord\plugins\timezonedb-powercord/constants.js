b8fbf4160bca0aead37c7a723a4ad5d04b32322c
Liz Ainslie <lizainslie16@gmail.com>
2022-08-19 14:08:04 -0700

    fix(powercord): didn't mean to upload that

--- platforms/powercord/C:\Users\lizai\Documents\powercord\src\Powercord\plugins\timezonedb-powercord/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	WEBSITE: 'https://tzdb.synapsetech.dev',
-	DISCORD_INVITE: 'https://discord.gg/7vtx32JgMM',
-	DEFAULT_FORMAT: 'h:MM TT',
-	DEFAULT_API_URL: 'https://tzdbapi.synapsetech.dev',
-};
